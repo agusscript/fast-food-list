f3110548bac9cde55656295b76822f9d36ed4bb3
agusscript <agus.sanchez.240@gmail.com>
2023-08-27 21:09:06 -0300

    Add exception handling when the data in json file is incorrect

--- AppWindowsForm/Forms/ViewFoodsForm.cs
+++ AppWindowsForm/Forms/ViewFoodsForm.cs
@@ -29,23 +29,30 @@ namespace AppWindowsForm.Forms
 
         private void EditFoodBtn_Click(object sender, EventArgs e)
         {
-            int idFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[0].Value.ToString());
-            string nameFood = foodsDataGrid.SelectedRows[0].Cells[1].Value.ToString();
-            int timeFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[2].Value.ToString());
-            int priceFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[3].Value.ToString());
-            int caloriesFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[4].Value.ToString());
-            bool veganFood = bool.Parse(foodsDataGrid.SelectedRows[0].Cells[5].Value.ToString());
-
-            Food foodToModify = new Food(idFood, nameFood, timeFood, priceFood, caloriesFood, veganFood);
-
-            EditFoodForm editFoodForm = new EditFoodForm(foodToModify);
-            DialogResult dialogResult = editFoodForm.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            try
             {
-                FoodRepository.UpdateFood(idFood, editFoodForm.EditedFood);
-                ShowFoodsDataGrid(JsonFile.Data);
-                UpdateFoodsCount();
+                int idFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[0].Value.ToString());
+                string nameFood = foodsDataGrid.SelectedRows[0].Cells[1].Value.ToString();
+                int timeFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[2].Value.ToString());
+                int priceFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[3].Value.ToString());
+                int caloriesFood = int.Parse(foodsDataGrid.SelectedRows[0].Cells[4].Value.ToString());
+                bool veganFood = bool.Parse(foodsDataGrid.SelectedRows[0].Cells[5].Value.ToString());
+
+                Food foodToModify = new Food(idFood, nameFood, timeFood, priceFood, caloriesFood, veganFood);
+
+                EditFoodForm editFoodForm = new EditFoodForm(foodToModify);
+                DialogResult dialogResult = editFoodForm.ShowDialog();
+
+                if (dialogResult == DialogResult.OK)
+                {
+                    FoodRepository.UpdateFood(idFood, editFoodForm.EditedFood);
+                    ShowFoodsDataGrid(JsonFile.Data);
+                    UpdateFoodsCount();
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Empty or incorrect format of data in Json file", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
@@ -70,12 +77,17 @@ namespace AppWindowsForm.Forms
         private void ShowFoodsDataGrid(string file)
         {
             string foodsData = File.ReadAllText(file);
-
             FoodRepository.InitializeRepository();
 
-            FoodRepository.Foods = JsonSerializer.Deserialize<List<Food>>(foodsData);
-
-            foodsDataGrid.DataSource = FoodRepository.Foods;
+            try
+            {
+                FoodRepository.Foods = JsonSerializer.Deserialize<List<Food>>(foodsData);
+                foodsDataGrid.DataSource = FoodRepository.Foods;
+            }
+            catch (JsonException ex)
+            {
+                MessageBox.Show("Empty or incorrect format of data in Json file", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void UpdateFoodsCount()
